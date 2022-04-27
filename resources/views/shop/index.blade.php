b1e16f91afdd3f08618cee9156a02c324c39ca6d
Amoy N <moyanich@users.noreply.github.com>
2022-04-27 08:20:22 -0500

    Update css

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -11,7 +11,12 @@
 				
 					<a href="#" class="group">
 						<div class="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
-							<img src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg" alt="Tall slender porcelain bottle with natural clay textured body and cork stopper." class="w-full h-full object-center object-cover group-hover:opacity-75">
+						
+							@if($product->image_path)
+								<img src="{{ asset($product->image_path) }}" alt="{{ $product->name }}" class="w-full h-full object-center object-cover group-hover:opacity-75">
+							@endif
+
+							<img src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg" alt="{{ $product->name }}" class="w-full h-full object-center object-cover group-hover:opacity-75">
 						</div>
 						<h3 class="mt-4 text-sm text-gray-700">{{ $product->name }}</h3>
 						<p class="mt-1 text-lg font-medium text-gray-900">${{ $product->price }}</p>
